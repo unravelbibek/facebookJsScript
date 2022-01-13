25d40121a1c91ffdbc86bb3cfa09cc34616651c4
whoissubedibibek <subedibibek00@outlook.com>
2022-01-14 05:38:39 +0545

    facebook script

---
+++ index.js
@@ -0,0 +1,6 @@
+var toBeClicked = document.querySelectorAll("div.hedtx66o.lgfrwdfx.cbu4d94t.j83agx80 i.hu5pjgll.m6k467ps");
+lengthtoBeClicked = toBeClicked .length;
+
+for( i=0; i< lengthtoBeClicked; i++ ){
+	toBeClicked[i].click();
+}
