33bc9d131b9b7cbc945e88943e4fb26467c5c6fa
whoissubedibibek <subedibibek00@outlook.com>
2022-01-14 05:55:50 +0545

    optimized for page works for everyone

---
+++ indexPage.js
@@ -0,0 +1,7 @@
+
+var toBeClicked = document.querySelectorAll("div.rdhft8ur.a31nu0t8.r8a9u66l.ni8dbmo4.stjgntxs.k4urcfbm div.bp9cbjyn.j83agx80.btwxx1t3 div div div i.hu5pjgll.m6k467ps");
+lengthtoBeClicked = toBeClicked .length;
+
+for( i=0; i< lengthtoBeClicked; i++ ){
+	toBeClicked[i].click();
+}
